92f80476cb41f0b1338f096704952b48c0c9c7ae
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-10-31 11:02:25 -0400

    Update FactorialCounter(BenM)/FactorialCounter(BenM)/FactorialForm.cs

--- FactorialCounter(BenM)/FactorialCounter(BenM)/FactorialForm.cs
+++ FactorialCounter(BenM)/FactorialCounter(BenM)/FactorialForm.cs
@@ -15,19 +15,36 @@ namespace FactorialCounter_BenM_
         public frmFactorial()
         {
             InitializeComponent();
+            this.lblFactorialAns.Hide();
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             //Declares variables
-            double aNumb, factorialNumb;
+            double aNumb, factorialAns;
+            int factorialCounter;
 
             //Clears list box
             this.lstFactorials.Items.Clear();
-       
 
-            
+            //Get numb from user
+            aNumb = Convert.ToDouble(txtNumb.Text);
 
+            factorialAns = 1;
+
+            factorialCounter = 0;
+            //Gets the factorials
+            do
+            {
+                factorialCounter++;
+                lstFactorials.Items.Add(factorialCounter);
+                factorialAns = factorialCounter * factorialAns;
+
+            } while (factorialCounter < aNumb);
+
+            //Displays stuff in the label
+            this.lblFactorialAns.Show();
+            this.lblFactorialAns.Text = this.txtNumb.Text + "! = " + Convert.ToString(factorialAns);
 
         }
     }
