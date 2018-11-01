1698613b699174001c10007fc4cf869b79d654e3
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-01 10:59:35 -0400

    Unicode

---
+++ Unicode(BenM)/Unicode(BenM)/UnicodeForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Unicode_BenM_
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbs = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumbs
+            // 
+            this.lstNumbs.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbs.FormattingEnabled = true;
+            this.lstNumbs.ItemHeight = 18;
+            this.lstNumbs.Location = new System.Drawing.Point(75, 182);
+            this.lstNumbs.Name = "lstNumbs";
+            this.lstNumbs.Size = new System.Drawing.Size(161, 184);
+            this.lstNumbs.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(139, 65);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 44);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 441);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumbs);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbs;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Unicode(BenM)/Unicode(BenM)/UnicodeForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Ben Motz
+ * Created on: 1-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 24# - Unicode
+ * This program display the character's number in unicode
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unicode_BenM_
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Declares varaibles
+            int counter;
+            const int MAXNUMB = 90, MINNUMB = 65;
+            string character;
+
+            //Clears lstNumbs
+            this.lstNumbs.Items.Clear();
+
+            //Diplays stuff in listbox
+            for (counter = MINNUMB; counter <= MAXNUMB; counter++)
+            {
+                character = char.ConvertFromUtf32(counter);
+                lstNumbs.Items.Add(character + "->" + counter);
+
+            }
+        }
+    }
+}
