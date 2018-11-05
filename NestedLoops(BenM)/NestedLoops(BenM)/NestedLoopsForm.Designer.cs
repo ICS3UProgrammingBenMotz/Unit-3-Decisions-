b11b1911d4fdf61efd294d2f8234794f37e58377
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-05 11:03:52 -0500

    update

---
+++ NestedLoops(BenM)/NestedLoops(BenM)/NestedLoopsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NestedLoops_BenM_
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(148, 79);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 54);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(122, 165);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(170, 147);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 359);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoops";
+            this.Text = "NestedLoops";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ NestedLoops(BenM)/NestedLoops(BenM)/NestedLoopsForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Ben M
+ * Created on: 05-11-18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program displays letters from A to Z with their lowercase letter beside them
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoops_BenM_
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Creates Variables
+            int secondLetcounter,firstLetcounter;
+            string firstLet, secondLet;
+
+            //Clears listbox
+            this.lstLetters.Items.Clear();
+
+            //Creates loop
+            for (firstLetcounter = 65; firstLetcounter <= 90; firstLetcounter++)
+            {
+                firstLet = char.ConvertFromUtf32(firstLetcounter);
+                //Creates loop for second letter
+                for (secondLetcounter=97; secondLetcounter<=122;secondLetcounter++)
+                {
+                    secondLet = char.ConvertFromUtf32(secondLetcounter);
+                    this.lstLetters.Items.Add(firstLet + " -> " + secondLet);
+
+                }
+                
+            }
+        }
+    }
+}
