74296f01e9987f54f375cef5cfeab39dd32af7fb
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-27 19:06:51 -0500

    uodate

---
+++ WhileLoop(BenM)/WhileLoop(BenM)/WhileLoopForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WhileLoop_BenM_
+{
+    partial class frmWhileLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.lblEnterPerfectSquare = new System.Windows.Forms.Label();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(183, 96);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudNumber.TabIndex = 0;
+            // 
+            // lblEnterPerfectSquare
+            // 
+            this.lblEnterPerfectSquare.AutoSize = true;
+            this.lblEnterPerfectSquare.Location = new System.Drawing.Point(199, 54);
+            this.lblEnterPerfectSquare.Name = "lblEnterPerfectSquare";
+            this.lblEnterPerfectSquare.Size = new System.Drawing.Size(82, 13);
+            this.lblEnterPerfectSquare.TabIndex = 1;
+            this.lblEnterPerfectSquare.Text = "Enter a number!";
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(183, 182);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(132, 147);
+            this.lstPerfectSquares.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(183, 134);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(101, 29);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmWhileLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 451);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.lblEnterPerfectSquare);
+            this.Controls.Add(this.nudNumber);
+            this.Name = "frmWhileLoop";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.Label lblEnterPerfectSquare;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ WhileLoop(BenM)/WhileLoop(BenM)/WhileLoopForm.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by: Ben M
+ * Created on: 27-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22- WhileLoop
+ * This program calculates the perfect square of a certain numb
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhileLoop_BenM_
+{
+    public partial class frmWhileLoop : Form
+    {
+        public frmWhileLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Creates varaibles
+            int endVal;
+            int val;
+            Double squareRAsDoub;
+            int squareRAsInt;
+
+            //Makes val = 1
+            val = 1;
+
+            //Clears items for lst box
+            this.lstPerfectSquares.Items.Clear();
+
+            //gets user's entry
+            endVal = Convert.ToInt32(nudNumber.Value);
+
+            //creates while loop
+            while (val<=endVal)
+            {
+                //square roots val
+                squareRAsDoub = Math.Sqrt(val);
+
+                //converts to a int32
+                squareRAsInt = Convert.ToInt32(squareRAsDoub);
+
+                if (squareRAsDoub==squareRAsInt)
+                {
+                    //adds it ot lst box
+                    this.lstPerfectSquares.Items.Add(val + " is a perfect square");
+
+                    //refresh form
+                    this.Refresh();
+                }
+                //increments val by 1
+                val++;
+
+            }
+        }
+    }
+}
